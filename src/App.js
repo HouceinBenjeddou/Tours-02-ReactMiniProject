00004884117fff563b2367accbe853ee3ab9d286
HouceinBenjeddou <hou52cine@gmail.com>
2022-11-17 22:25:47 +0100

    Initial-commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+const url = 'https://course-api.com/react-tours-project'
+
+function App() {
+  const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([]);
+
+   const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+   }
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const respone = await fetch(url);
+      const tours = await respone.json();
+      setLoading(false)
+      setTours(tours)
+    } catch (err) {
+      setLoading(false)
+      console.log(err);
+    }
+  };
+  
+  useEffect( () => {
+    fetchTours();
+  }, [])
+
+  if(loading){
+    return (
+    <main>
+      <Loading />
+    </main>
+  );
+}
+  if(tours.length === 0 ){
+    return (
+      <main>
+        <div className='title'>
+          <h2>no Tours Left</h2>
+          <button className='btn' onClick={fetchTours}>Refrech</button>
+        </div>
+      </main>
+    )
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour}/>
+    </main>
+  );
+}
+
+export default App
